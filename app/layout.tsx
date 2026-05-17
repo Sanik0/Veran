90114afbb859076730d40d04be2339efb0d69f34
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-05-17 17:59:30 +0800

    removed custom css from globals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/dist/client/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+       <Script src="/js/templatemo-axis-script.js" strategy="lazyOnload" />
     </html>
   );
 }
